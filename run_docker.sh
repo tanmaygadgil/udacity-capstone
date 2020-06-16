fb25be5863790fac96e0b5a647511bcda9835239
tanmay-p-gadgil <tanmay-p-gadgil@pwc.com>
2020-06-16 14:57:47 +0530

    first commit

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t udacity-project-4:v2 .
+docker build -t udacity-capstone .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -it -d -p 8000:80 udacity-project-4:v2
+docker run -it -d -p 8000:80 udacity-capstone
